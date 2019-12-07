c62c61e97d9804465712785500888f12373e3a89
unknown <jihadchama@gmail.com>
2019-12-07 17:13:54 -0400

    corrigido isPossibleMove da CommandTower e adicionado alguns testes

--- src/br/com/jihad/sonda/CommandTowerTest.java
+++ src/br/com/jihad/sonda/CommandTowerTest.java
@@ -1,5 +1,29 @@
-import static org.junit.jupiter.api.Assertions.*;
+package br.com.jihad.sonda.test;
+
+import br.com.jihad.sonda.CommandTower;
+import br.com.jihad.sonda.model.Coordinate;
+import br.com.jihad.sonda.model.Directions;
+import br.com.jihad.sonda.planet.Planet;
+import br.com.jihad.sonda.vehicle.Sonda;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 class CommandTowerTest {
 
+    @Test
+    public void shouldReturnTrueWhenSondaIsPossibleToLand(){
+
+        Coordinate coordinate = new Coordinate(6, 6);
+        Coordinate sondaCoordinate = new Coordinate(5, 5);
+        Planet planet = new Planet(coordinate);
+        Sonda sonda = new Sonda();
+
+        CommandTower commandTower = new CommandTower();
+
+        commandTower.launch(sonda, sondaCoordinate, 'N', planet);
+
+        assertTrue(commandTower.isPossibleLand(sonda, sondaCoordinate, planet));
+    }
 }
